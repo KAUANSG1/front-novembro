f27460abca85d1387d602241d32b80abe1e70a5b
KAUANSG1 <kauanseixass@gmail.com>
2024-11-29 21:32:39 -0300

    Bug Jquery Corrigido

--- aulas_curso_fullstack/form_em_bootstrap.js
+++ aulas_curso_fullstack/form_em_bootstrap.js
@@ -2,11 +2,28 @@ $(document).ready(function(){
 
     $("#bt-cadastro").click(function(){
 
+        $("#nome").removeClass("is-invalid");
+        $("#cidade").removeClass("is-invalid");
+        $("[name=sexo]").removeClass("is-invalid");
+
+
         var nome = $("#nome").val();
-        if (nome == "")
+        if (nome.trim() == "")
         {
             $("#nome").addClass("is-invalid");
-        } 
+        }
+
+        if ($("#cidade").val() == "0")
+        {
+            $("#cidade").addClass("is-invalid");
+        }
+
+        if ($("[name=sexo]:checked").length == 0)
+        {
+            $("[name=sexo]").addClass("is-invalid");
+        }
+
+        
 
     }); // Fim do botão
 
